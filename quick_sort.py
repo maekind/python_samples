5f9bca37781d377ef09b20e48a79b49d1a6833d5
marco <marco@marcoespinosa.es>
2023-02-12 18:37:15 +0100

    Create class for Quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -1,54 +1,73 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python3
-""" Python program for implementation of Quicksort Sort
-This implementation utilizes pivot as the last element in the nums list
-It has a pointer to keep track of the elements smaller than the pivot
-At the very end of partition() function, the pointer is swapped with the pivot
-to come up with a "sorted" nums relative to the pivot """
+""" This file contains the Quicksort algorithm implementation """
 
 
-def partition(array, low, high):
-    """ Function to find the partition position """
+class QuickSort():
+    """ This implementation utilizes pivot as the last element in the nums list
+    It has a pointer to keep track of the elements smaller than the pivot
+    At the very end of partition() function, the pointer is swapped with the pivot
+    to come up with a "sorted" nums relative to the pivot """
 
-    # choose the rightmost element as pivot
-    pivot = array[high]
+    def __init__(self, array):
+        """ Default constructors 
 
-    # pointer for greater element
-    i = low - 1
+            params: 
+                @array: Array of numbers
+        """
 
-    # traverse through all elements
-    # compare each element with pivot
-    for j in range(low, high):
-        if array[j] <= pivot:
+        self._data = array
 
-            # If element smaller than pivot is found
-            # swap it with the greater element pointed by i
-            i = i + 1
+    def run(self):
+        """ Launches quicksort algorithm """
+        self._quick_sort(self._data, 0, len(self._data) - 1)
 
-            # Swapping element at i with element at j
-            (array[i], array[j]) = (array[j], array[i])
+    def _partition(self, array, low, high):
+        """ Function to find the partition position """
 
-    # Swap the pivot element with the greater element specified by i
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
+        # choose the rightmost element as pivot
+        pivot = array[high]
 
-    # Return the position from where partition is done
-    return i + 1
+        # pointer for greater element
+        i = low - 1
 
+        # traverse through all elements
+        # compare each element with pivot
+        for j in range(low, high):
+            if array[j] <= pivot:
 
-def quick_sort(array, low, high):
-    """ Function to perform the quicksort """
-    if low < high:
+                # If element smaller than pivot is found
+                # swap it with the greater element pointed by i
+                i = i + 1
 
-        # Find pivot element such that
-        # element smaller than pivot are on the left
-        # element greater than pivot are on the right
-        pivot = partition(array, low, high)
+                # Swapping element at i with element at j
+                (array[i], array[j]) = (array[j], array[i])
 
-        # Recursive call on the left of pivot
-        quick_sort(array, low, pivot - 1)
+        # Swap the pivot element with the greater element specified by i
+        (array[i + 1], array[high]) = (array[high], array[i + 1])
 
-        # Recursive call on the right of pivot
-        quick_sort(array, pivot + 1, high)
+        # Return the position from where partition is done
+        return i + 1
+
+    def _quick_sort(self, array, low, high):
+        """ Function to perform the quicksort """
+        if low < high:
+
+            # Find pivot element such that
+            # element smaller than pivot are on the left
+            # element greater than pivot are on the right
+            pivot = self._partition(array, low, high)
+
+            # Recursive call on the left of pivot
+            self._quick_sort(array, low, pivot - 1)
+
+            # Recursive call on the right of pivot
+            self._quick_sort(array, pivot + 1, high)
+
+    @property
+    def data(self):
+        """" Property data """
+        return self._data
 
 
 def test_sorted_array_in_ascendant_order():
@@ -58,20 +77,24 @@ def test_sorted_array_in_ascendant_order():
     unsorted = [1, 7, 4, 1, 10, 23, 9, -2]
     expected = [-2, 1, 1, 4, 7, 9, 10, 23]
 
-    quick_sort(unsorted, 0, len(unsorted) - 1)
+    # Quicksort instance
+    qci = QuickSort(unsorted)
+    qci.run()
 
-    assert expected == unsorted
+    assert expected == qci.data
 
 
 def main():
     """ Main function """
 
-    data = [1, 7, 4, 1, 10, 23, 9, -2]
-    print(f"Unsorted Array: {data}")
+    unsorted = [1, 7, 4, 1, 10, 23, 9, -2]
+    print(f"Unsorted Array: {unsorted}")
 
-    quick_sort(data, 0, len(data) - 1)
+    # Quicksort instance
+    qci = QuickSort(unsorted)
+    qci.run()
 
-    print(f'Sorted Array in Ascending Order: {data}')
+    print(f'Sorted Array in Ascending Order: {qci.data}')
 
 
 if __name__ == "__main__":
