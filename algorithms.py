29c3f2f37f93d0553d807bbf28b7a7a3f0eef67b
marco <marco@marcoespinosa.es>
2023-02-12 17:08:06 +0100

    Add rich comments

--- algorithms.py
+++ algorithms.py
@@ -7,7 +7,7 @@
 
 def search_employees_manager(employee, managers):
     """ Search the employee's manager in a dictionary """
-    # The dictionary is a manager:[employees] key pair.
+    # The dictionary is a manager:[employees] key-value pair.
     employee_cleaned = employee.strip().lower().title()
     for manager, employees in managers.items():
         if employee_cleaned in employees:
@@ -32,9 +32,13 @@ def swap_dict_a_b_values(values):
 def swap_dict_positions(values, pos1, pos2):
     """ Swap two positions in a dictionary """
     try:
+        # Convert values to list
         tuple_values = list(values.items())
 
+        # Swap positions, as in swap_dict_a_b_values
         tuple_values[pos1], tuple_values[pos2] = tuple_values[pos2], tuple_values[pos1]
+
+        # Convert back to a dictionary
         values = dict(tuple_values)
     except (TypeError, IndexError) as error:
         print(error)
